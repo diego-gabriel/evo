e67b47522875d7c70b9c347f9754d3e921e40f3a
isaacsoto <isaacsotomar@gmail.com>
2014-04-27 07:39:00 -0700

    Let's Fight!

--- www/js/FightController.js
+++ www/js/FightController.js
@@ -1,14 +1,23 @@
-function FightController(riot){
+function FightController(riot, policePack){
 	this.riot = riot;
+	this.policePack = policePack;
 }
-//Aqui solo se recibe por el momento a un policia (ustedes dijeron por el momento) 
-function fight(police){
+
+function fight(){
 	while(riot.exists){
-		var i;
-		for(i=1; i<riot.rioters.length + 1; i++){
-			riot.liveController();
-			riot.rioters[Math.floor((Math.random()*i))].attack();
-			setTimeout(function(){police.attack()},300);
-		}				
+		Concurrent.Thread.riotersAttack(function(){
+			for(var i=1; i<riot.rioters.length + 1; i++){
+				riot.liveController();
+				riot.rioters[Math.floor((Math.random()*i))].attack();
+			}
+		});
+		//setTimeout(function(){police.attack()},300);
+		Concurrent.Thread.policePackAttack(function(){
+			for(var i=1; i<policePack.polices.length + 1; i++){
+				riot.liveController();
+				policePack.polices[Math.floor((Math.random()*i))].attack();
+			}
+		});
+		
 	}
 }
