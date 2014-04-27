68707f56ca0cf77674cca8a762b53be3c7e8912f
= <davidzonneveld@gmail.com>
2014-04-27 11:13:49 -0400

    refactored file system

---
+++ www/js/mapGenerator.js
@@ -0,0 +1,33 @@
+var mapGenerator = {
+	generateMap : function (polygons) {
+		console.log("POLYGONS: " + polygons);
+		for (var polygon in polygons) {
+			console.log(polygon);
+			drawpolygon(polygons[polygon]);	
+		};
+	}
+}
+
+var drawpolygon = function (polygon) {
+	var svg = $("#map"),
+	r=0,g=100,b=200;	
+	var myPoints = '';
+	for (var row in polygon) {		
+		for (var column in polygon[row]) {
+			myPoints+= polygon[row][column].x;
+			myPoints+= ',';
+			myPoints+= polygon[row][column].y;
+			myPoints += " ";
+		}		
+		$("<polygon points=\""+myPoints+"\" style=\"fill:rgb("+r+","+g+","+b+");stroke:grey;stroke-width:20\"/>").appendTo("svg");
+		r+=10;
+		g+=10;
+		b-=10;		
+		myPoints = '';
+	}
+	$("#cont").html($("#cont").html());
+	console.log(svg);
+	console.log(myPoints);	
+}
+
+mapGenerator.generateMap(levels[1]["polygons"]);
