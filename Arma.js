19a3689b521cc383986f38e817cbef2d33672e9c
fmarcelosur <fmarcelo.sur@gmail.com>
2014-04-26 21:51:42 -0400

    añadidas las clases de arma, bloqueo y policia

---
+++ Arma.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function Arma(precio, rango, rangoVelocidadRecarga, nivelMinimo){
+	this.precio = 0;
+	this.rango = 0;
+	this.rangoVelocidadRecarga = 0;
+	this.nivelMinimo = 0;
+	
+	
+}
---
+++ Policia.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+
+function Police(grado, minAtaque, maxAtaque, minRecarga,maxRecarga,velocidad, nivelArma,experiencia, precioMin,precioMax ) {
+	 
+	 this.grado = grado;
+	 
+	 this.minVida = 0;
+	 
+	 this.maxVida = 100;
+	
+	 this.minAtaque =  minAtaque;
+	
+	 this.maxAtaque = maxAtaque;
+	 
+	 this.minRecarga = minRecarga;
+	 
+	 this.maxRecarga = maxRecarga;
+	 
+	 this.velocidad = velocidad;
+	 
+	 this.nivelArma = nivelArma;
+	 
+	 this.experiencia = experiencia;
+	 
+	 this.precioMin = precioMin;
+	 
+	 this.precioMax = precioMax;	
+	
+}
