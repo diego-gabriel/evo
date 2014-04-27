6c7becbbe186852fcab05dda9021ab663625f004
Teo Panda <antonio@publiaciertos.net>
2014-04-27 10:48:48 -0400

    agregado mapa 2

--- www/data/levels.js
+++ www/data/levels.js
@@ -59,3 +59,61 @@ levels[0]["polygons"][8][0] = levels["puntos"][2][2];
 levels[0]["polygons"][8][1] = levels["puntos"][2][3];
 levels[0]["polygons"][8][2] = levels["puntos"][3][3];
 levels[0]["polygons"][8][3] = levels["puntos"][3][2];
+
+levels[1] = new Array();
+levels[1]["polygons"] = new Array();
+levels[1]["polygons"] = [[],[],[],[],[],[],[],[],[]];
+levels[1]["polygons"][0][0] = levels["puntos"][0][0];
+levels[1]["polygons"][0][1] = levels["puntos"][1][0];
+levels[1]["polygons"][0][2] = levels["puntos"][2][0];
+levels[1]["polygons"][0][3] = levels["puntos"][1][1];
+levels[1]["polygons"][0][4] = levels["puntos"][0][2];
+levels[1]["polygons"][0][5] = levels["puntos"][0][1];
+
+levels[1]["polygons"][1][0] = levels["puntos"][0][2];
+levels[1]["polygons"][1][1] = levels["puntos"][1][1];
+levels[1]["polygons"][1][2] = levels["puntos"][2][0];
+levels[1]["polygons"][1][3] = levels["puntos"][2][1];
+levels[1]["polygons"][1][4] = levels["puntos"][2][2];
+levels[1]["polygons"][1][5] = levels["puntos"][2][3];
+levels[1]["polygons"][1][6] = levels["puntos"][2][4];
+levels[1]["polygons"][1][7] = levels["puntos"][1][4];
+levels[1]["polygons"][1][8] = levels["puntos"][0][4];
+levels[1]["polygons"][1][9] = levels["puntos"][0][3];
+
+/*levels[1]["polygons"][2][0] = levels["puntos"][2][0];
+levels[1]["polygons"][2][1] = levels["puntos"][3][1];
+levels[1]["polygons"][2][2] = levels["puntos"][1][3];
+levels[1]["polygons"][2][3] = levels["puntos"][1][2];
+levels[1]["polygons"][2][4] = levels["puntos"][1][2];
+levels[1]["polygons"][2][5] = levels["puntos"][1][2];
+
+levels[1]["polygons"][3][0] = levels["puntos"][1][0];
+levels[1]["polygons"][3][1] = levels["puntos"][1][1];
+levels[1]["polygons"][3][2] = levels["puntos"][2][1];
+levels[1]["polygons"][3][3] = levels["puntos"][2][0];
+
+levels[1]["polygons"][4][0] = levels["puntos"][1][1];
+levels[1]["polygons"][4][1] = levels["puntos"][1][2];
+levels[1]["polygons"][4][2] = levels["puntos"][2][2];
+levels[1]["polygons"][4][3] = levels["puntos"][2][1];
+
+levels[1]["polygons"][5][0] = levels["puntos"][1][2];
+levels[1]["polygons"][5][1] = levels["puntos"][1][3];
+levels[1]["polygons"][5][2] = levels["puntos"][2][3];
+levels[1]["polygons"][5][3] = levels["puntos"][2][2];
+
+levels[1]["polygons"][6][0] = levels["puntos"][2][0];
+levels[1]["polygons"][6][1] = levels["puntos"][2][1];
+levels[1]["polygons"][6][2] = levels["puntos"][3][1];
+levels[1]["polygons"][6][3] = levels["puntos"][3][0];
+
+levels[1]["polygons"][7][0] = levels["puntos"][2][1];
+levels[1]["polygons"][7][1] = levels["puntos"][2][2];
+levels[1]["polygons"][7][2] = levels["puntos"][3][2];
+levels[1]["polygons"][7][3] = levels["puntos"][3][1];
+
+levels[1]["polygons"][8][0] = levels["puntos"][2][2];
+levels[1]["polygons"][8][1] = levels["puntos"][2][3];
+levels[1]["polygons"][8][2] = levels["puntos"][3][3];
+levels[1]["polygons"][8][3] = levels["puntos"][3][2];*/
--- www/usr/scripts/mapGenerator.js
+++ www/usr/scripts/mapGenerator.js
@@ -6,10 +6,7 @@ var mapGenerator = {
 }
 var drawpolygon = function (polygon) {
 	var svg = $("#map"),
-	r=0,g=100,b=200;
-	//var svg1 = document.getElementById("map");
-	//var div = $("#prueba");
-	//var div1 = document.getElementById("prueba");
+	r=0,g=100,b=200;	
 	var myPoints = '';
 	for (var row in polygon) {		
 		for (var column in polygon[row]) {
@@ -17,28 +14,7 @@ var drawpolygon = function (polygon) {
 			myPoints+= ',';
 			myPoints+= polygon[row][column].y;
 			myPoints += " ";
-		}
-		/*var polygonHtml =
-		jQuery("<polygon>", {
- 		   "points": myPoints,
- 		   "style":"fill:lime;stroke:grey;stroke-width:10"
-		}).appendTo("svg");*/
-		//var poly= makeSVG('circle', {cx: 100, cy: 50, r:40, stroke: 'black', 'stroke-width': 2, fill: 'red'});
-		//svg1.appendChild(poly);
-     /*svg.append(jQuery("<polygon>", {
- 		   "points": myPoints,
- 		   "style":"fill:lime"
-		}));*/
-		/*jQuery("<p>", { 		   
- 		   "style":"fill:lime"
-		}).appendTo("div");*/
-		/*var iNombre = document.createElement("polygon");
-		iNombre.setAttribute("points",myPoints);	
-		iNombre.setAttribute("style","fill:lime;stroke:grey;stroke-width:20");
-		svg1.appendChild(iNombre);
-		iDiv = document.createElement("p");
-		div1.appendChild(iDiv);*/		
-		
+		}		
 		$("<polygon points=\""+myPoints+"\" style=\"fill:rgb("+r+","+g+","+b+");stroke:grey;stroke-width:20\"/>").appendTo("svg");
 		r+=10;
 		g+=10;
@@ -50,4 +26,4 @@ var drawpolygon = function (polygon) {
 	console.log(myPoints);	
 }
 
-mapGenerator.generateMap(levels[0]["polygons"]);
+mapGenerator.generateMap(levels[1]["polygons"]);
